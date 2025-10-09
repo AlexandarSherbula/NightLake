98eedb57b61ed31d35819f11a44766656402b6c0
AlexandarSherbula <acabor90@gmail.com>
2025-10-09 19:50:20 +0200

    Hopefully fixed the redefenition error

--- Engine/Source/Alexio.hpp
+++ Engine/Source/Alexio.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-
-#include "Utils/Math.hpp"
 #include "Core/Application.hpp"
 #include "Utils/Log.hpp"
 #include "Utils/Utilities.hpp"
 
+#include "Utils/Math.hpp"
 #include "Events/Events.hpp"
 #include "Input/Input.hpp"
 #include "Window/Window.hpp"
