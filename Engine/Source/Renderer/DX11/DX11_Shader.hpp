fa5f6ea3a01fc963fbd9d122d204ae91871f6008
AlexandarSherbula <acabor90@gmail.com>
2025-10-15 23:37:54 +0200

    Forgot to uncomment empty definitions for Linux

--- Engine/Source/Renderer/DX11/DX11_Shader.hpp
+++ Engine/Source/Renderer/DX11/DX11_Shader.hpp
@@ -47,40 +47,29 @@ namespace aio
 #else
 namespace aio
 {
-	//class DX11_Shader : public Shader
-	//{
-	//public:
-	//	DX11_Shader(const std::string& name, const Ref<VertexInput>& vertexInput) {}
-	//	DX11_Shader(const std::string& name, const std::string& filepath, const Ref<VertexInput>& vertexInput) {}
-	//	DX11_Shader(const std::string& name, const std::string& vertexSrc, const std::string& pixelSrc, const Ref<VertexInput>& vertexInput) {}
-	//	~DX11_Shader() {}
-	//
-	//
-	//	void Bind() const override {}
-	//	void Unbind() const override {}
-	//
-	//	void SetInt(const std::string& name, int32_t value) override {}
-	//	void SetIntArray(const std::string& name, int32_t* values, uint32_t count) override {}
-	//	void SetFloat(const std::string& name, float value) override {}
-	//	void SetFloat2(const std::string& name, const Vector2& value) override {}
-	//	void SetFloat3(const std::string& name, const Vector3& value) override {}
-	//	void SetFloat4(const std::string& name, const Vector4& value) override {}
-	//
-	//	void SetMat3x3(const std::string& name, const glm::mat3x3& matrix) override {}
-	//	void SetMat4x4(const std::string& name, const Mat4x4& matrix) override {}
-	//private:
-	//	void Compile(const Ref<VertexInput>& vertexInput) {}
-	//private:
-	//	std::string mVertexSource;
-	//	std::string mPixelSource;
-	//
-	//	Microsoft::WRL::ComPtr<ID3D11VertexShader> mVertexShader;
-	//	Microsoft::WRL::ComPtr<ID3DBlob> mVertexShaderBuffer;
-	//
-	//	Microsoft::WRL::ComPtr<ID3D11PixelShader> mPixelShader;
-	//	Microsoft::WRL::ComPtr<ID3DBlob> mPixelShaderBuffer;
-	//
-	//	Microsoft::WRL::ComPtr<ID3D11InputLayout> mVertexLayout;
-	//};
+	class DX11_Shader : public Shader
+	{
+	public:
+		DX11_Shader(const std::string& name, const Ref<VertexInput>& vertexInput) {}
+		DX11_Shader(const std::string& name, const std::string& filepath, const Ref<VertexInput>& vertexInput) {}
+		DX11_Shader(const std::string& name, const std::string& vertexSrc, const std::string& pixelSrc, const Ref<VertexInput>& vertexInput) {}
+		~DX11_Shader() {}
+	
+	
+		void Bind() const override {}
+		void Unbind() const override {}
+	
+		void SetInt(const std::string& name, int32_t value) override {}
+		void SetIntArray(const std::string& name, int32_t* values, uint32_t count) override {}
+		void SetFloat(const std::string& name, float value) override {}
+		void SetFloat2(const std::string& name, const Vector2& value) override {}
+		void SetFloat3(const std::string& name, const Vector3& value) override {}
+		void SetFloat4(const std::string& name, const Vector4& value) override {}
+	
+		void SetMat3x3(const std::string& name, const glm::mat3x3& matrix) override {}
+		void SetMat4x4(const std::string& name, const Mat4x4& matrix) override {}
+	private:
+		void Compile(const Ref<VertexInput>& vertexInput) {}
+	};
 }
 #endif
